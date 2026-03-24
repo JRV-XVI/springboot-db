3f4efc3acbe24829d806d3aac501641b36ed308b
Ángel Gabriel Camacho Pérez <141674071+AngelGabrielCP5TEC@users.noreply.github.com>
2026-03-23 20:49:39 -0600

    bd pokemon

--- db/src/main/java/com/springboot/db/controller/TestDBController.java
+++ db/src/main/java/com/springboot/db/controller/TestDBController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.db.controller.TestDBController;
 import com.springboot.db.service.FirebaseService;
 
+import java.util.Random;
+
 @RestController
 public class TestDBController {
     private final FirebaseService firebaseService;
+    private final Random random = new Random();
 
     public TestDBController(FirebaseService firebaseService) {
         this.firebaseService = firebaseService;
@@ -20,6 +23,14 @@ public class TestDBController {
 
         System.out.println("Holaaaaa");
         firebaseService.guardarDato();
+
+        
+        //pruebas de actualizar vidas de pokemones
+        int vida1 = random.nextInt(100);
+        int vida2 = random.nextInt(100);
+        System.out.println("Actualizando vidas: " + vida1 + " y " + vida2);
+        firebaseService.actualizarVidas(vida1, vida2);
+
         return "Dato enviado a Firebase";
     }
 }
